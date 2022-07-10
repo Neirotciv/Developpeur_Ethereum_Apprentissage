acdf9fd477eb98c86e179c936638507ee5da0c52
VicLam <tanacetifolia@gmail.com>
2022-07-10 18:18:30 +0200

    natspecs for contracts

--- 4-Truffle_&_CI-CD/1-Truffle_project/contracts/SimpleStorage.sol
+++ 4-Truffle_&_CI-CD/1-Truffle_project/contracts/SimpleStorage.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
- 
+
+/** @title Shape calculator. */
 contract SimpleStorage {
    uint data;
- 
-   function set(uint x) public {
-       data = x;
-   }
- 
-   function get() public view returns (uint) {
-       return data;
-   }
+
+    /** @dev Calculates a rectangle's surface and perimeter.
+      * @param  x of the rectangle.
+      */
+    function set(uint x) public {
+        data = x;
+    }
+    
+    /** @dev Calculates a rectangle's surface and perimeter.
+      * @return data The calculated surface.
+      */
+    function get() public view returns (uint) {
+        return data;
+    }
 }
